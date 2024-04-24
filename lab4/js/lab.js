e2128b55ccf0b5e2c0d20386f5a5a7da882f28cb
halberd69 <hfung2015@gmail.com>
2024-04-23 18:23:45 -0700

    Lab 4 Pseudocode

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,8 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+
+// Rock Paper Scissors
+// An implementation of the game
+// Wes Modes <wmodes@ucsc.edu>
+// April 2024
 
 // Constants
 
@@ -16,6 +18,14 @@ function main() {
   console.log("Main function started.");
   // the code that makes everything happen
 }
-
+							//Establish win/loss conditions – rock triumphs over scissors triumphs over paper triumphs over rock.
+							//Set up gameboard into thirds, opponent’s side, your side, and UI with rock paper scissor icons as choice selections.
+							//Computer countdowns for players to make a selection between the three icons.
+							//Accept user input
+							//Player chooses either rock, paper, or scissors
+							//Computer compares the player’s choice to the opponent’s choice
+							//Declaration of who wins that round
+							//Repeat until the game has been played 3 times
+              
 // let's get this party started
 main();
