7020d47f9c9bec89980137547ef2383c3de42439
halberd69 <hfung2015@gmail.com>
2024-05-20 01:26:32 -0700

    lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,21 +7,18 @@
 // Functions
 
 
-// click listener for button
-$("#submit").click(function(){
-  // get value of input field
-const userName = $("#user-name").val();
-});
-
+// Sorts the characters of a string in alphabetical order.
 function sortString(inputString) {
-  
   // We have to convert our string to an array and back again to sort it
   return inputString.split('').sort().join('');
 }
 
-// now let's sort it
-userNameSorted = sortString(userName);
-
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + userNameSorted + '</p></div>');
+// click listener for button
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+  // now let's sort it
+  userNameSorted = sortString(userName);
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
